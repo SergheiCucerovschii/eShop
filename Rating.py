1894c39bc6cda44487bf32615092cb38ed2ca674
SergheiCucerovschii <74251573+SergheiCucerovschii@users.noreply.github.com>
2021-06-23 21:48:22 +0300

    Update Rating.py

--- Rating.py
+++ Rating.py
@@ -1,5 +1,5 @@
-# from Customer import *
-# from Product import *
+from Customer import *
+from Product import *
 
 class Rating:
     def __init__(self, id, productId, customerId,stars,review):
@@ -9,22 +9,6 @@ class Rating:
         self.stars = stars
         self.review = review
 
-
-    def starsOverall(self):
-        while True:
-            try:
-                userInput = int(input("Enter a star rating between 0 and 5"))
-                if userInput >= 0 and userInput <= 5:
-                    print("Thank You")
-                    break
-            except ValueError:
-                print("Invalid star rating! Try again.")
-                continue
-            return self.stars
-
-    def rewiewText(self):
-        text = input("Your review")
-        return self.review
 
     def __str__(self):
         return f"{self.id} {self.productId} {self.customerId} Rating - {self.stars} Rewiew: {self.review}"
