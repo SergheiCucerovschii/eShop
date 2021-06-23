3148a7dc7e5d21add4de8bca678e1f636edc4936
SergheiCucerovschii <74251573+SergheiCucerovschii@users.noreply.github.com>
2021-06-23 21:30:09 +0300

    Add files via upload

---
+++ Rating.py
@@ -0,0 +1,38 @@
+# from Customer import *
+# from Product import *
+
+class Rating:
+    def __init__(self, id, productId, customerId,stars,review):
+        self.id = id
+        self.productId = productId
+        self.customerId = customerId
+        self.stars = stars
+        self.review = review
+
+
+
+
+    def starsOverall(self):
+        while True:
+            try:
+                userInput = int(input("Enter a star rating between 0 and 5"))
+                if userInput >= 0 and userInput <= 5:
+                    print("Thank You")
+                    break
+            except ValueError:
+                print("Invalid star rating! Try again.")
+                continue
+            return self.stars
+
+    def rewiewText(self):
+        text = input("Your review")
+        return self.review
+
+    def __str__(self):
+        return f"{self.id} {self.productId} {self.customerId} Rating - {self.stars} Rewiew: {self.review}"
+
+    def __repr__(self):
+        return self.__str__()
+
+e = Rating(6,8, john,{self.stars}, {self.review})
+print(e)
